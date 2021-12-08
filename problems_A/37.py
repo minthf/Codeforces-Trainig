77dabbeacea22819f0fb7ba134a39bdbb654e652
Mintimir <kachaev.666@gmail.com>
2021-12-08 22:47:56 +0600

    Problem A #37

---
+++ problems_A/37.py
@@ -0,0 +1,14 @@
+n = max(list(map(int, input().split())))
+
+if n == 6:
+    print('1/6')
+elif n == 5:
+    print('1/3')
+elif n == 4:
+    print('1/2')
+elif n == 3:
+    print('2/3')
+elif n == 2:
+    print('5/6')
+elif n == 1:
+    print('1/1')
