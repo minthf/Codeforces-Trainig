1a46056e0452c10d73ee6aed43e5753482593f0c
Mintimir <kachaev.666@gmail.com>
2022-01-19 13:07:06 +0600

    Problem B #48

--- problems_B/48.py
+++ problems_B/48.py
@@ -1,1 +1,17 @@
-pass
+n = int(input())
+arr = list(map(int, input().split()))
+maxi = max(arr)
+ind = arr.index(maxi)
+b = True
+for i in range(n-1):
+    if i < ind:
+        if arr[i] > arr[i + 1]:
+            b = False
+    if i > ind:
+        if arr[i] < arr[i + 1]:
+            b = False
+
+if b:
+    print('YES')
+else:
+    print('NO')
